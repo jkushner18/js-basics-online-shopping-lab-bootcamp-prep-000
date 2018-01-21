122a344559cad77857da6082b60811fd18ad8040
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-21 23:40:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,42 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+
+var itemPrice=Math.floor(Math.random()*100);
+
+var newCartItem = {}
+
+newCartItem[item]=itemPrice
+
+cart.push(newCartItem)
+
+console.log(item+" has been added to your cart.")
+
+return cart
+
 }
 
 function viewCart() {
-  // write your code here
+  
+for(var i = 0;i<=cart.length ;i++){
+  
+var statement = "In your cart, you have "
+  
+if (cart.length===0)  {
+  
+console.log("Your shopping cart is empty.")  
+
 }
+
+else if(cart.length==1){
+
+console.log("'"+ statement + Object.keys(cart[i])+ " at " + "$" + cart[i][Object.keys(cart[i])[0]]+ "." + "'") }
+  
+else {console.log("nope")}
+  
+}
+  } 
+
 
 function total() {
   // write your code here
